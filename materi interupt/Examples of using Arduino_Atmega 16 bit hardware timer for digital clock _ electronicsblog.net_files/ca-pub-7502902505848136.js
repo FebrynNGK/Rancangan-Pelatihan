645aeee6316d1edc7aca743bae6022eb6ec87226
FebrynNGK <newfebryn@gmail.com>
2016-02-24 06:27:24 +0700

    file yang salah copy

---
+++ materi interupt/Examples of using Arduino_Atmega 16 bit hardware timer for digital clock _ electronicsblog.net_files/ca-pub-7502902505848136.js
@@ -0,0 +1,1 @@
+(google_async_config = window.google_async_config || {})['ca-pub-7502902505848136'] = {"sra_enabled":false};
---
+++ materi interupt/Timer dan Counter Mikrokontroler _ Geyosoft.com_files/ca-pub-2044266494217305.js
@@ -0,0 +1,1 @@
+(google_async_config = window.google_async_config || {})['ca-pub-2044266494217305'] = {"sra_enabled":true};
